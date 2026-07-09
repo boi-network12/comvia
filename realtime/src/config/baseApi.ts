73833c4567b68d59d1294de6fb6334250390061d
okolo kamdilichukwu samuel <150658859+boi-network12@users.noreply.github.com>
2026-07-09 01:20:43 +0100

    ..

--- realtime/src/config/baseApi.ts
+++ realtime/src/config/baseApi.ts
@@ -2,4 +2,4 @@
 
 export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 
-export const API_URL = process.env.API_URL || 'https://comvia-backend-endpoint.vercel.app/api';
+export const API_URL = 'https://comvia-backend-endpoint.vercel.app/api';
